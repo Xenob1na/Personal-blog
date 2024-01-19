b1fe55b30cd79196c7374ef337e2b7ba4b337210
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2024-01-20 01:06:22 +0700

    Create Author Card

---
+++ components/AuthorCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="author-card">
+        <div class="container author-card__container">
+            <div class="author-card__profile">
+                <img src="/author.png" class="author-card__img" alt="author">
+            </div>
+            <div class="author-card__name">
+                <h1 class="author-card__title">TOMAS LAURINAVICIUS</h1>
+            </div>
+            <div class="author-card__info">
+                <p class="author-card__description">Hi, my name is Daniel, I'm the CTO here at Kinsta.</p>
+            </div>
+            <div class="author-card__social-media">
+                <a href="#" class="author-card__link">
+                    <Icon name="ri:github-fill" size="28" />
+                </a>
+                <a href="#" class="author-card__link">
+                    <Icon name="mdi:pinterest" size="28" />
+                </a>
+                <a href="#" class="author-card__link">
+                    <Icon name="mdi:instagram" size="28" />
+                </a>
+            </div>
+        </div>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        
-    </div>
+    <section class="Main">
+        <AuthorCard />
+    </section>
 </template>
