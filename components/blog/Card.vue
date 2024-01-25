5731ee5ccab82a70098cae7948b792e570519c19
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2024-01-25 20:07:02 +0700

    Started working on blog-item

--- components/blog/Card.vue
+++ components/blog/Card.vue
@@ -1,9 +1,11 @@
 <template>
     <section class="blog-card">
         <div class="blog-card__container">
-            <div class="blog-card__img">
-                <img :src="card.image" alt="blog" class="card-img">
-            </div>
+            <nuxt-link :to="`blog-${card.id}`">
+                <div class="blog-card__img">
+                    <img :src="card.image" alt="blog" class="card-img">
+                </div>
+            </nuxt-link>
             <div class="blog-card__content content">
                 <p class="content__author-name">BY {{ card.authorName }}</p>
                 <h3 class="content__title">{{ card.title }}</h3>
---
+++ pages/blog-[id].vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="blog-item">
+        <div class="container blog-item__container">
+            <div class="blog-item__content-header content-header">
+                <h1 class="content-header__title">10 Reasons to Build Your Website with WP Page Builder</h1>
+                <p class="content-header__description">People’s quest for creating websites has easily taken us to a new era
+                    of
+                    site development. Where, with the availability of robust page building tools, creating websites has
+                    become a lot more fun (especially for non-developers).</p>
+                <p class="content-header__author-name">BY TOMAS LAURINAVICIUS</p>
+            </div>
+            <div class="blog-item__content-main content-main">
+                <div class="content-main__post-img">
+                    <img src="/post.png" alt="">
+                </div>
+                <p class="content-main__description">People’s quest for creating websites has easily taken us to a new era
+                    of site development. Where, with the availability of robust page building tools, creating websites has
+                    become a lot more fun (especially for non-developers). The multitude of tools and plugins available to
+                    you is vast when you try building websites on WordPress. Today we’ll explore a new one, WP Page Builder.
+                    If you’re tired of the same old page builder plugins, this is one you should try out.</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
